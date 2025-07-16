3cec6880a10b46c27a92f250778a98df4a4f04d2
Ahmet <berkahmet91@gmail.com>
2025-07-16 09:43:46 +0300

    Update vite.config.js ,(API)
    API çekebilmek için publicDir olarak değişti

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/Belediye_Web/",
+  publicDir: "public",
   plugins: [react()],
 });
